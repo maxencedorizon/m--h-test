a2521717a85cdd544f3613ed3fff0dd804559e80
XoneLaasTV <66334045+maxencedorizon@users.noreply.github.com>
2021-10-19 11:47:53 +0000

    base of the clock

---
+++ index.js
@@ -0,0 +1,19 @@
+var end = new Date('5/22/2017 11:59 PM');
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+
+function clock(){
+    var now = new Date();
+    var distance = end - now;
+    var hours = Math.floor(distance / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    console.log('début de la clock')
+    console.log(hours + ' hr restantes..');
+    console.log(minutes + ' min restantes..');
+    console.log(seconds + ' sec restantes..');
+}
+
+var countdownTimer = setInterval(clock, 1000);
